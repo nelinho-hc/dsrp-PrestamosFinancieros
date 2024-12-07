3c04ee1992a1ec96f79ab8b7d14fef035b318821
Nelinho Hurtado <nelinho.hurtado@pucp.edu.pe>
2024-12-07 14:04:03 -0500

    26/11/2024

--- create_database/crear_bd.sql
+++ create_database/crear_bd.sql
@@ -1,2 +1,32 @@
 CREATE DATABASE dsrp_prestamos_financieros_6;
 GO
+
+USE dsrp_prestamos_financieros_6;
+GO
+-- Personas Naturales
+CREATE TABLE personas_naturales(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	numero_documento VARCHAR(20) UNIQUE NOT NULL,
+	nombres VARCHAR(255) NOT NULL,
+	apellido_paterno VARCHAR(255) NOT NULL,
+	apellido_materno VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+	celular VARCHAR(20) NOT NULL,
+	direccion VARCHAR(255) NOT NULL
+);
+GO
+
+SELECT * FROM personas_naturales;
+
+EXEC SP_HELP personas_naturales;
+
+-- Personas Jurídicas
+CREATE TABLE personas_juridicas(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	numero_documento VARCHAR(20) UNIQUE NOT NULL,
+	razon_social VARCHAR(255) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	telefono VARCHAR(20) NOT NULL,
+	direccion VARCHAR(255) NOT NULL
+);
+GO
